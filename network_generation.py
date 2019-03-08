c05167d452755ffbc4caa3f5990f407309420dc0
Siobhan <spowell2@stanford.edu>
2019-03-08 12:54:44 -0800

    Added loads and sgens into six_bus network creation with initialized values from config

--- network_generation.py
+++ network_generation.py
@@ -4,12 +4,13 @@ import pandapower as pp
 def get_net(config):
     if config.env_name == 'Six_Bus_POC':
         return six_bus(config.vn_high, config.vn_low, config.length_km,
-                       config.std_type, config.battery_locations, config.init_socs, config.energy_capacities)
+                       config.std_type, config.battery_locations, config.init_socs,
+                       config.energy_capacities, config.static_feeds)
 
 
 def six_bus(vn_high=20, vn_low=0.4, length_km=0.03, std_type='NAYY 4x50 SE', battery_locations=[3, 6], init_soc=0.5,
-            energy_capacity=20.0):
-    net = pp.create_empty_network(name='6bus', f_hz=60., sn_kva=100., )
+            energy_capacity=20.0, static_feeds=None):
+    net = pp.create_empty_network(name='6bus', f_hz=60., sn_kva=100.)
     # create buses
     for i in range(8):
         nm = 'bus{}'.format(i)
@@ -45,5 +46,15 @@ def six_bus(vn_high=20, vn_low=0.4, length_km=0.03, std_type='NAYY 4x50 SE', bat
     for i in range(num_batteries):
         pp.create_storage(net, bus=battery_locations[i], p_kw=0.0, max_e_kwh=energy_capacity, soc_percent=init_soc)
     #  add loads and static generation
-    
+    if static_feeds is None:
+        print('No loads or generation assigned to network')
+    else:
+        if len(static_feeds) > 0:
+            for key, val in static_feeds.items():
+                init_flow = val[0]
+                if init_flow > 0:
+                    pp.create_load(net, bus=key, p_kw=init_flow, q_kvar=0)
+                else:
+                    pp.create_sgen(net, bus=key, p_kw=init_flow, q_kvar=0)
+
     return net
