db74263fc4c03f2b57e32a820a44de0ca151d680
Bennet Meyers <bennetm@stanford.edu>
2019-03-07 16:11:31 -0800

    adding configuration details to set up the network and store the static feeds of non-controllable powers

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
+import numpy as np
+
+
 class config_sixbus_poc:
     def __init__(self, use_baseline):
         self.env_name = 'Six_Bus_POC'
-
+        
         # output config
-        self.output_path = "results/{}-{}/".format(self.env_name, baseline_str)
+        baseline_str      = 'baseline' if use_baseline else 'no_baseline'
+        self.output_path  = "results/{}-{}/".format(self.env_name, baseline_str)
         self.model_output = self.output_path + "model.weights/"
         self.log_path     = self.output_path + "log.txt"
         self.plot_output  = self.output_path + "scores.png"
         self.record_path  = self.output_path
-        self.record_freq = 5
+        self.record_freq  = 5
         self.summary_freq = 1
 
         # model and training config
@@ -20,6 +24,18 @@ class config_sixbus_poc:
         self.use_baseline           = use_baseline
         self.normalize_advantage    = True
 
+        # environment generation
+        self.vn_high = 20
+        self.vn_low = 0.4
+        self.length_km = 0.03
+        self.std_type = 'NAYY 4x50 SE'
+        self.static_feeds = {
+            3: -10 * np.ones(self.max_ep_len),
+            6: -10 * np.ones(self.max_ep_len),
+            4: 10 * np.ones(self.max_ep_len),
+            7: 10 * np.ones(self.max_ep_len)
+        }
+
         # parameters for the policy and baseline models
         self.n_layers               = 1
         self.layer_size             = 16
@@ -29,3 +45,8 @@ class config_sixbus_poc:
         assert self.max_ep_len <= self.batch_size
         if self.max_ep_len < 0:
             self.max_ep_len = self.batch_size
+
+
+def get_config(env_name, baseline):
+    if env_name == 'Six_Bus_POC':
+        return config_sixbus_poc(baseline)
