79020d9ed6cdfd2dcd666f740264dc85a0eb5581
Bennet Meyers <bennetm@stanford.edu>
2019-03-14 19:28:53 -0700

    adding replay buffer

--- ddpg.py
+++ ddpg.py
@@ -15,6 +15,7 @@ import scipy.signal
 import os
 import time
 import inspect
+from collections import deque
 
 from pp_network import NetModel
 from utils.general import get_logger, Progbar, export_plot
@@ -27,6 +28,55 @@ parser.add_argument('--baseline', dest='use_baseline', action='store_true')
 parser.add_argument('--no-baseline', dest='use_baseline', action='store_false')
 parser.set_defaults(use_baseline=True)
 
+
+class ReplayBuffer(object):
+  """
+  A data structure to hold the replay buffer. Based on this blog post:
+  https://pemami4911.github.io/blog/2016/08/21/ddpg-rl.html
+  """
+  def __init__(self, buffer_size):
+    self.buffer_size = buffer_size
+    self.count = 0
+    self.buffer = deque()
+
+  def add(self, s, a, r, t, s2):
+    experience = (s, a, r, t, s2)
+    if self.count < self.buffer_size:
+      self.buffer.append(experience)
+      self.count += 1
+    else:
+      self.buffer.popleft()
+      self.buffer.append(experience)
+
+  def size(self):
+    return self.count
+
+  def sample_batch(self, batch_size):
+    '''
+    batch_size specifies the number of experiences to add
+    to the batch. If the replay buffer has less than batch_size
+    elements, simply return all of the elements within the buffer.
+    Generally, you'll want to wait until the buffer has at least
+    batch_size elements before beginning to sample from it.
+    '''
+    batch = []
+
+    if self.count < batch_size:
+      batch = random.sample(self.buffer, self.count)
+    else:
+      batch = random.sample(self.buffer, batch_size)
+
+    s_batch = np.array([_[0] for _ in batch])
+    a_batch = np.array([_[1] for _ in batch])
+    r_batch = np.array([_[2] for _ in batch])
+    t_batch = np.array([_[3] for _ in batch])
+    s2_batch = np.array([_[4] for _ in batch])
+
+    return s_batch, a_batch, r_batch, t_batch, s2_batch
+
+  def clear(self):
+    self.buffer.clear()
+    self.count = 0
 
 def build_actor(actor_input, output_size, scope, n_layers, size, min_p, max_p):
   """
