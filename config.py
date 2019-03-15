fe6f1f8674ebc7c4fa7f8704a7ca7e4a05090d0d
Bennet Meyers <bennetm@stanford.edu>
2019-03-14 19:27:25 -0700

    adding new configurations for ddpg

--- config.py
+++ config.py
@@ -23,14 +23,25 @@ class ConfigSixBusPOC(object):
         self.record_freq  = 5
         self.summary_freq = 1
 
-        # model and training config
+        # model and training - general
+        self.gamma                  = 0.9 # the discount factor
+
+        # model and training config - PG
         self.num_batches            = 500 # number of batches trained on
         self.batch_size             = 1000 # number of steps used to compute each policy update
         self.max_ep_len             = 60 # maximum episode length
         self.learning_rate          = 3e-2
-        self.gamma                  = 0.9 # the discount factor
         self.use_baseline           = use_baseline
         self.normalize_advantage    = True
+
+        # model and training config - DDPG
+        self.tau                    = 0.001
+        self.actor_learning_rate    = 1e-3
+        self.critic_learning_rate   = 1e-2
+        self.buffer_size            = 1e6
+        self.minibatch_size         = 64
+        self.max_episodes           = 500
+        self.max_ep_steps           = 60
 
         # environment generation
         self.tstep = 1. / 60
@@ -97,14 +108,25 @@ class StandardLVNetwork(object):
         self.record_freq = 5
         self.summary_freq = 1
 
-        # model and training config
-        self.num_batches = 100  # number of batches trained on
-        self.batch_size = 1000  # number of steps used to compute each policy update
-        self.max_ep_len = 60  # maximum episode length
-        self.learning_rate = 3e-2
-        self.gamma = 0.9  # the discount factor
-        self.use_baseline = use_baseline
-        self.normalize_advantage = True
+        # model and training - general
+        self.gamma                  = 0.9  # the discount factor
+
+        # model and training config - PG
+        self.num_batches            = 500  # number of batches trained on
+        self.batch_size             = 1000  # number of steps used to compute each policy update
+        self.max_ep_len             = 60  # maximum episode length
+        self.learning_rate          = 3e-2
+        self.use_baseline           = use_baseline
+        self.normalize_advantage    = True
+
+        # model and training config - DDPG
+        self.tau                    = 0.001
+        self.actor_learning_rate    = 1e-3
+        self.critic_learning_rate   = 1e-2
+        self.buffer_size            = 1e6
+        self.minibatch_size         = 64
+        self.max_episodes           = 500
+        self.max_ep_steps           = 60
 
         self.remove_q = True
         self.clear_loads_sgen = False
