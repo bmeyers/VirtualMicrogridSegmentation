9b5bcad832c474493e2afa6caa95c84da1316b50
Bennet Meyers <bennetm@stanford.edu>
2019-03-14 20:52:51 -0700

    adding an implementation of Orstein-Uhlenbech random process for exploration

--- ddpg.py
+++ ddpg.py
@@ -112,6 +112,32 @@ def build_actor(actor_input, output_size, scope, n_layers, size, min_p, max_p):
 
   return out
 
+class OrnsteinUhlenbeckActionNoise(object):
+  """
+  Implementation of an Ornstein–Uhlenbeck process for exploration. Based on:
+  https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py
+  https://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+  """
+  def __init__(self, mu, sigma, theta=.15, dt=1e-2, x0=None):
+    self.theta = theta
+    self.mu = mu
+    self.sigma = sigma
+    self.dt = dt
+    self.x0 = x0
+    self.reset()
+
+  def __call__(self):
+    x = (self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt +
+         self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape))
+    self.x_prev = x
+    return x
+
+  def reset(self):
+    self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
+
+  def __repr__(self):
+    return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu,
+                                                            self.sigma)
 
 def build_critic(mlp_input, actions_input, scope, n_layers, size):
   """
