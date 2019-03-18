76e8a48bf830e6c50695c81111e236d5b7b8817c
Bennet Meyers <bennetm@stanford.edu>
2019-03-17 19:49:43 -0700

    updating algorithm scripts

--- scripts/run_pg.py
+++ scripts/run_pg.py
@@ -3,11 +3,11 @@ sys.path.append('..')
 
 from virtual_microgrids.configs import get_config
 from virtual_microgrids.powerflow import NetModel
-from virtual_microgrids.algorithms import DDPG
+from virtual_microgrids.algorithms import PG
 
 if __name__ == '__main__':
-    config = get_config('Six_Bus_POC', algorithm='DDPG')
+    config = get_config('Six_Bus_POC', algorithm='PG')
     env = NetModel(config=config)
     # train model
-    model = DDPG(env, config)
+    model = PG(env, config)
     model.run()
